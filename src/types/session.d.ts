f9d0e83b54f06fc5478f95400b857f89d063d25a
Weyd <weydlll2606@gmail.com>
2026-06-09 17:05:52 -0300

    correção do ession.d.ts

--- src/types/session.d.ts
+++ src/types/session.d.ts
@@ -2,8 +2,8 @@ import "express-session";
 
 declare module "express-session" {
   interface SessionData {
-    userId?: number;
-    userName?: string;
-    flash?: string | null;
+    flash: string | null;
+    userId: number;
+    userName: string;
   }
 }
